225a692976af163b88527aacb23a8d33fce4135f
estelita1 <22670068@iguala.tecnm.mx>
2023-02-10 06:55:02 -0600

    Practica1 terminada - estela santiago

---
+++ src/suma.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class suma {
+    public static void main(String[] args) {
+
+        int num1 = 20;
+        int num2 = 89;
+
+        int operacion = num1+num2;
+        System.out.println( "la suma es " + operacion);
+    }
+}
